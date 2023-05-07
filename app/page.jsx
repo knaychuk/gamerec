56054ecb816cbc2fa3bec0c63dbaae63217c29ba
Kate <knaychuk@gmail.com>
2023-05-06 21:21:24 -0700

    file structure + imports

---
+++ app/page.jsx
@@ -0,0 +1,16 @@
+import Feed from '@components/Feed';
+
+const Home = () => {
+  return (
+    <section>
+      <h1 className="header-text text-center">Discover & Share
+        <br className="max-md:hidden"/>
+        <span>Game Recommondations or Other Info</span>
+      </h1>
+      <p className="text-center">GameRec is an open-source forum to share all things game related!</p>
+
+      <Feed />
+    </section>
+  )
+}
+export default Home
