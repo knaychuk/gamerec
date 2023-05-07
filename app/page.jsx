3f179b04dd39bf6f2854846a3c17c165b7446b10
Kate <knaychuk@gmail.com>
2023-05-06 22:46:01 -0700

    navbar items

--- app/page.jsx
+++ app/page.jsx
@@ -4,7 +4,7 @@ const Home = () => {
   return (
     <section>
       <h1 className="header-text text-center">Discover & Share
-        <br className="max-md:hidden"/>
+        <br className=""/>
         <span>Game Recommondations or Other Info</span>
       </h1>
       <p className="text-center">GameRec is an open-source forum to share all things game related!</p>
--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,11 +1,54 @@
-import Link from 'next.link';
+"use client";
+
+import Link from 'next/link';
 import Image from 'next/image';
 import { useState, useEffect } from 'react';
 import { signIn, signOut, useSession, getProviders } from 'next-auth/react';
 
 const Nav = () => {
+  const isUserLoggedIn = true;
+
+  const [providers, setProviders] = useState(null);
+
+  useEffect(() => {
+    const setProviders = async () => {
+      const response = await getProviders();
+
+      setProviders(response);
+    }
+
+    setProviders();
+  }, [])
+
   return (
-    <div>Nav</div>
+    <nav>
+      <Link href="/">
+        
+      </Link>
+
+      {/* mobile nav */}
+      <div>
+        {isUserLoggedIn &&
+          <div>
+            <Link href="/create-prompt" className='p-2 bg-primary'>Create Post</Link>
+            <button >Sign Out</button>
+            <Link href="/profile">Profile</Link>
+          </div> 
+          
+        }
+        {!isUserLoggedIn &&
+          <div>
+            {providers && 
+              Object.values(providers).map((provider) => (
+                <button type="button" key={provider.name} onClick={() => signIn(provider.id)}>
+
+                </button>
+              )
+            )}
+          </div>
+        }
+      </div>
+    </nav>
   )
 }
 export default Nav
