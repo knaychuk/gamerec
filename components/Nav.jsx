fd9f568757bb14c89bfd8b8ac33a39a3d85e1b47
Kate <knaychuk@gmail.com>
2023-05-07 22:31:25 -0700

    front-end auth setup

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -6,7 +6,7 @@ import { useState, useEffect } from 'react';
 import { signIn, signOut, useSession, getProviders } from 'next-auth/react';
 
 const Nav = () => {
-  const isUserLoggedIn = true;
+  const isUserLoggedIn = false;
 
   const [providers, setProviders] = useState(null);
 
@@ -26,12 +26,11 @@ const Nav = () => {
         
       </Link>
 
-      {/* mobile nav */}
       <div>
         {isUserLoggedIn &&
           <div>
             <Link href="/create-prompt" className='p-2 bg-primary'>Create Post</Link>
-            <button >Sign Out</button>
+            <button onClick={() => {signOut();}}>Sign Out</button>
             <Link href="/profile">Profile</Link>
           </div> 
           
@@ -40,8 +39,8 @@ const Nav = () => {
           <div>
             {providers && 
               Object.values(providers).map((provider) => (
-                <button type="button" key={provider.name} onClick={() => signIn(provider.id)}>
-
+                <button type="button" key={provider.name} onClick={() => signIn(provider.id)} className='bg-cyan-400'>
+                  Sign In
                 </button>
               )
             )}
