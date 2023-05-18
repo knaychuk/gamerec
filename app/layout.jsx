e8a378bb1c82b6e688e8ba462ccf8f652af7f798
Kate <knaychuk@gmail.com>
2023-05-17 18:27:50 -0700

    added favicon

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,15 +2,29 @@ import '@styles/globals.css';
 
 import Nav from '@components/Nav';
 import Provider from '@components/Provider';
+import Head from 'next/head';
 
 export const metadata = {
   title: 'GameRec',
   description: 'Share Game Recommondations With Others',
+  icons: {
+    icon: '/icon.png',
+    shortcut: '/shortcut-icon.png',
+    apple: '/apple-icon.png',
+    other: {
+      rel: 'apple-touch-icon-precomposed',
+      url: '/apple-touch-icon-precomposed.png',
+    },
+  },
 }
  
 export default function RootLayout({ children }) {
  return (
     <html lang="en">
+
+<link rel="icon" href="/icon.png" />
+
+    
       <body className='font-inter font-bold text-primary-t'>
         <Provider>
         <div className='main'>
